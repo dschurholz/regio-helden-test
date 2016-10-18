51829a0ada339b7d81b508222a58f20fb46ebfcf
dschurholz <daniel.schurholz@gmail.com>
2016-10-17 23:15:15 -0500

    Adding the customer (CRUD user) model with only last name, first name and IBAN fields.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import Customer
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_by", )
+    list_display = ("iban", "first_name", "last_name")
+    search_fields = ("first_name", "last_name", "iban")
+
+    def save_model(self, request, obj, form, change):
+        obj.created_by = request.user
+        obj.save()
+
+
+admin.site.register(Customer, CustomerAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from django_iban.fields import IBANField
+
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=32, db_index=True)
+    last_name = models.CharField(max_length=32, db_index=True)
+    iban = IBANField(
+        unique=True,
+        help_text="Up to 34 chars bank account number, depends on country.")
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True,
+        on_delete=models.SET(get_sentinel_user),
+        related_name='customers_added')
